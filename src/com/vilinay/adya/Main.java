275f87573d42dae0f911d38385d0c149f4e8a5d9
Vinay Phadnis <phadnisvinay30@gmail.com>
2018-01-23 22:50:57 +0530

    updated java now

--- src/com/vilinay/adya/Main.java
+++
@@ -1,9 +0,0 @@
-package com.vilinay.adya;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hey there welcome to the adya repo");
-    }
-}
